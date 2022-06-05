d0bbd1b2116eca98ca987750782f7603b2d2e42f
Tyler Sprankle <spranklemiami@gmail.com>
2022-06-05 07:40:10 -0400

    nav margin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-const lImgC = document.querySelector("#landing-image-c");
-const landingImg = document.createElement("img");
-landingImg.setAttribute("src", "file:///Users/pyronumeric/Desktop/102492.jpeg");
-landingImg.style = "width: 100%;";
-lImgC.append(landingImg);
+// const landingPageC = document.querySelector("#landing-page-c");
+// const landingImg = document.createElement("img");
+// landingImg.setAttribute("src", "file:///Users/pyronumeric/Desktop/102492.jpeg");
+// landingImg.style = "width: 100%;";
+// landingPageC.append(landingImg);
