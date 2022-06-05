4e587c58519be4b794a4572f02054f3cde261cb3
Tyler Sprankle <spranklemiami@gmail.com>
2022-06-05 07:22:08 -0400

    initial js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const lImgC = document.querySelector("#landing-image-c");
+const landingImg = document.createElement("img");
+landingImg.setAttribute("src", "file:///Users/pyronumeric/Desktop/102492.jpeg");
+landingImg.style = "width: 100%;";
+lImgC.append(landingImg);
