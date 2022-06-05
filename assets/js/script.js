852860cdf81cb19920521fc802fdbbfa351cb3ba
Tyler Sprankle <spranklemiami@gmail.com>
2022-06-05 13:04:09 -0400

    bg-img test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-// const landingPageC = document.querySelector("#landing-page-c");
-// const landingImg = document.createElement("img");
-// landingImg.setAttribute("src", "./assets/images/102492.jpeg");
-// landingImg.style = "width: 100%;";
-// landingPageC.append(landingImg);
+const landingPageC = document.querySelector("#landing-page-c");
+const landingImg = document.createElement("img");
+landingImg.setAttribute("src", "./assets/images/102492.jpeg");
+landingImg.style = "width: 100%;";
+landingPageC.append(landingImg);
+let apptQEl = document.createElement("div").setAttribute("id", "appt-q");
+
+
+// let apptQtext = document.createElement("h4");
+// apptQtext.textContent("Would you like to schedule an appointment?");
+// document.body.appendChild(apptQtext);
+// document.body.append(apptQEl);
