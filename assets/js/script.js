89b5a3ad6f1c8a2dc7b630f1ed94e5e8054fde0e
Tyler Sprankle <spranklemiami@gmail.com>
2022-06-05 19:44:28 -0400

    font-size

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,15 @@ const landingImg = document.createElement("img");
 landingImg.setAttribute("src", "./assets/images/102492.jpeg");
 landingImg.style = "width: 100%;";
 landingPageC.append(landingImg);
-let apptQEl = document.createElement("div").setAttribute("id", "appt-q");
 
 
-// let apptQtext = document.createElement("h4");
-// apptQtext.textContent("Would you like to schedule an appointment?");
-// document.body.appendChild(apptQtext);
-// document.body.append(apptQEl);
+
+
+let testDiv = document.createElement("div");
+testDiv.setAttribute("id", "test-div");
+// testDiv.style = "position: absolute; border: 1px solid red; width: 100px; height: 100px;";
+testDiv.addEventListener("mouseover", function() {
+    alert("who");
+});
+document.body.append(testDiv);
+
