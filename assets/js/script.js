07bdaf97ff182beef1d0882f75243818b03e5cf5
Tyler Sprankle <spranklemiami@gmail.com>
2022-06-05 08:13:49 -0400

    landing image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-// const landingPageC = document.querySelector("#landing-page-c");
-// const landingImg = document.createElement("img");
-// landingImg.setAttribute("src", "file:///Users/pyronumeric/Desktop/102492.jpeg");
-// landingImg.style = "width: 100%;";
-// landingPageC.append(landingImg);
+const landingPageC = document.querySelector("#landing-page-c");
+const landingImg = document.createElement("img");
+landingImg.setAttribute("src", "./assets/images/102492.jpeg");
+landingImg.style = "width: 100%;";
+landingPageC.append(landingImg);
